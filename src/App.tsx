fa5afc8e02781ca76349068e643aa6c17e35d8a8
MinhYi <minhyii.dev@gmail.com>
2024-08-12 11:51:02 +0700

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
     document.title = "Hello world";
 
     return (
-        <>
-            <h1>Hello world</h1>
-        </>
+        <Routes>
+            <Route path="/" element={<Navbar />} />
+            <Route path="/net" element={"Net"} />
+            <Route path="*" element={"Not Found"} />
+        </Routes>
     );
 }
 
