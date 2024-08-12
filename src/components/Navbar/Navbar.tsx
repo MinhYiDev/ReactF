be4fde3aaa5e7c826337795ad00b42f13bc99e76
MinhYi <minhyii.dev@gmail.com>
2024-08-12 12:40:38 +0700

    fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,4 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
 function Navbar() {
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        axios.get("http://54.169.142.60/net").then((data) => {
-            console.log(data.data);
-        });
-    }, []);
-
     return <div className="text-3xl">Navbar</div>;
 }
 
