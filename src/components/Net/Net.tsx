9cb5dd2ecba2fb173433c9466936d1de58e5413e
MinhYi <minhyii.dev@gmail.com>
2024-08-13 20:09:15 +0700

    .

--- src/components/Net/Net.tsx
+++ src/components/Net/Net.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 import "./index.scss";
 import axios from "axios";
-import moment from "moment-timezone";
 
 interface IDataNet {
     _id: string;
@@ -13,36 +12,70 @@ interface IDataNet {
 
 function Net() {
     const [result, setResult] = useState<IDataNet[]>([]);
-    const [time, setTime] = useState<string>("");
+    const [copy, setCopy] = useState<boolean>(false);
+    const contentCoppy: MutableRefObject<HTMLDivElement | null> =
+        useRef<HTMLDivElement>(null);
+
+    const handllerCopy = async () => {
+        if ("clipboard" in navigator) {
+            navigator.clipboard.writeText(
+                contentCoppy.current?.textContent as string
+            );
+            setCopy(true);
+        }
+
+        setTimeout(() => {
+            setCopy(false);
+        }, 3000);
+    };
 
     useEffect(() => {
         async function fetchData() {
             const res = await axios.get("https://api.psang.click/v1/api/net");
             setResult(res.data?.data);
-            const _time = moment(result[0]?.updatedAt).format(
-                "DD/MM/YYYY HH:mm:ss"
-            );
-            setTime(_time);
         }
         fetchData();
     }, []);
 
     return (
-        <div className="container__net">
+        <div className="container mt-4 container__net">
             <div className="title__net_lb">
-                <div>
-                    <div>
-                        Thời Gian Cập Nhật:😊
-                        {time}
-                    </div>
-                    <div>By P_SANG 😁</div>
+                <div className="wrap__title">
+                    <div className="text-5xl">Thời Gian Cập Nhật</div>
+                    <div className="text-4xl">By P_SANG 😁</div>
                 </div>
             </div>
 
-            <div className="box_parap">
-                {result.map((item, id) => (
-                    <p key={id}>{item.content}</p>
-                ))}
+            <div className="box__paragraph">
+                <div className="mt-4 flex justify-end mr-[6rem] p-4">
+                    <div
+                        onClick={() => handllerCopy()}
+                        className="flex cursor-pointer"
+                    >
+                        <button className="bg-slate-500 rounded-lg p-2 text-[#fff]">
+                            {copy ? "✅ Copied" : "Copy"}
+                        </button>
+                        <div className="ml-1 w-8 h-10">
+                            <svg
+                                aria-hidden="false"
+                                focusable="false"
+                                data-prefix="far"
+                                data-icon="copy"
+                                role="img"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 448 512"
+                                color="red"
+                            >
+                                <path d="M433.941 65.941l-51.882-51.882A48 48 0 0 0 348.118 0H176c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h224c26.51 0 48-21.49 48-48v-48h80c26.51 0 48-21.49 48-48V99.882a48 48 0 0 0-14.059-33.941zM266 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h74v224c0 26.51 21.49 48 48 48h96v42a6 6 0 0 1-6 6zm128-96H182a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h106v88c0 13.255 10.745 24 24 24h88v202a6 6 0 0 1-6 6zm6-256h-64V48h9.632c1.591 0 3.117.632 4.243 1.757l48.368 48.368a6 6 0 0 1 1.757 4.243V112z"></path>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div ref={contentCoppy} className="wrap__box_paragraph mt-4">
+                    {result.map((item, id) => (
+                        <p key={id}>{item.content}</p>
+                    ))}
+                </div>
             </div>
         </div>
     );
--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,8 +1,41 @@
+"use strict";
+import clsx from "clsx";
+import "./index.scss";
+import styles from "./NotFound.module.scss";
+import { RefObject, useRef, useState } from "react";
+
 function NotFound() {
+    const copyEle: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+    const [copied, setCopied] = useState<boolean>(false);
+    // const [copyText, setCopyText] = useState<string>();
+
+    const handllerCoppy = async () => {
+        if ("clipboard" in navigator) {
+            await navigator.clipboard.writeText(
+                copyEle.current?.textContent as string
+            );
+            setCopied(true);
+        }
+    };
+
+    setTimeout(() => {
+        setCopied(false);
+    }, 4000);
+
     return (
-        <div className="flex justify-center items-center flex-col h-[100vh]">
-            <div className="">404</div>
-            <div>NotFound</div>
+        <div className="flex sm: justify-center items-center flex-col h-[100vh]">
+            <section className="wrap__break_mobile flex">
+                <div ref={copyEle} className="bg-slate-500">
+                    <button
+                        onClick={handllerCoppy}
+                        className="relative left-[100%] hover:bg-red-500"
+                    >
+                        {copied ? "✅ Copied" : "Copy"}
+                    </button>
+                    <div className={clsx(styles.div)}>404</div>
+                    <div className="text-5xl font-bold">NotFound</div>
+                </div>
+            </section>
         </div>
     );
 }
