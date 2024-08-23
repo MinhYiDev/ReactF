fe2019d3c34e03573ff9bef605f840bc5ef9f747
MinhYi <minhyii.dev@gmail.com>
2024-08-23 22:41:31 +0700

    .

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
+import { persistor, store } from "./redux/store.ts";
 import { StrictMode } from "react";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     </StrictMode>
 );
