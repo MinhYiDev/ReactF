33c0e512678dd82035a77d9e45590214e8acc03e
MinhYi <minhyii.dev@gmail.com>
2024-08-26 21:10:33 +0700

    .

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,7 @@
 import { NavLink, Outlet } from "react-router-dom";
 import "./index.scss";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
 
 interface IContent {
     content: string;
@@ -7,16 +9,23 @@ interface IContent {
 }
 
 function Navigation(): JSX.Element {
+    const isLogin: boolean = useSelector(
+        (state: RootState) => state.auth.isSuccess
+    );
     const navC: IContent[] = [
         { content: "Trang Chủ", path: "/" },
         { content: "Thêm Net", path: "/post/net" },
         { content: "Cập Nhật", path: "/update/net" },
-        { content: "Login", path: "/login" },
     ];
+
+    const baseNav: IContent[] = isLogin
+        ? navC
+        : [...navC, { content: "Login", path: "/login" }];
+
     return (
         <>
             <div className="container mx-auto flex justify-center text-4xl">
-                {navC.map((item, index) => (
+                {baseNav.map((item, index) => (
                     <NavLink
                         className={({ isActive }) =>
                             isActive ? "underline text-red-500" : ""
